5af581034a328d27a752f9da716c8b98720c2e73
MaiHai <maihai290724@gmail.com>
2026-02-16 12:16:32 +0700

    bai_thuc_hanh_1

---
+++ MAX6675/src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+#include <max6675.h>
+
+int thermoSO = 19;
+int thermoCS = 5;
+int thermoSCK = 18;
+
+MAX6675 thermocouple(thermoSCK, thermoCS, thermoSO);
+
+void setup() {
+  Serial.begin(115200);
+  delay(500);
+  Serial.println("MAX6675 test");
+}
+
+void loop() {
+  float temperature = thermocouple.readCelsius();
+
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+
+  delay(1000);
+}
